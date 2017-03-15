13ea551841f55204f48280543a9db4a6256f42d6
skybluepro111 <TopFrontForward@mail.com>
2017-03-15 10:35:53 +0200

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganisationsPage } from '../pages/organisations/organisations';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
