ff9105a30e8f40995068fdccb459a371db166aac
skybluepro111 <TopFrontForward@mail.com>
2017-03-16 10:47:16 +0200

    finish searchbar

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -19,18 +19,34 @@ import { UserDetailsPage } from '../user-details/user-details';
 })
 export class UsersPage {
   users: User[]
+  originalUsers: User[];
 
   constructor(public navCtrl: NavController, private githubUsers: GithubUsers) {
     githubUsers.load().subscribe(users => {
       this.users = users;
+      this.originalUsers = users;
     })
     githubUsers.searchUsers('scotch').subscribe(users => {
         console.log(users)
-      });
+    });
   }
 
   goToDetails(login: string) {
     this.navCtrl.push(UserDetailsPage, {login});
+  }
+
+  search(searchEvent) {
+    let term = searchEvent.target.value
+    // We will only perform the search if we have 3 or more characters
+    if (term.trim() === '' || term.trim().length < 3) {
+      // Load cached users
+      this.users = this.originalUsers;
+    } else {
+      // Get the searched users from github
+      this.githubUsers.searchUsers(term).subscribe(users => {
+        this.users = users
+      });
+    }
   }
   
 }
